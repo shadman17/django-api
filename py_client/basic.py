1195e1966723504b04afd80ed9421ab3d5880d47
Kingsman <shadman.wasif@gmail.com>
2024-02-19 10:27:34 +0600

    GET data checking

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,11 +2,14 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/" #"http://127.0.0.1:8000/"
-endpoint1 = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint1, json={'query':'Hello World'}) #
-print(get_response.text)
+get_response = requests.get(endpoint, params={'abc' : 123}, json={"product_id": 123 }) # HTTP Request
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Nototion ~ Python Dict
 print(get_response.json())
-print(get_response.status_code)
-
+# print(get_response.status_code)
