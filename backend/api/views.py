bad4f19927bb54e98be4d5887c3408f116267520
Kingsman <shadman.wasif@gmail.com>
2024-02-20 10:42:52 +0600

    model_to_dict functionality check

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,13 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price']) 
+        return JsonResponse(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
