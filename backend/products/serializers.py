af72d02e7373f3c5a7d1648a5e5e59584eafa40e
Kingsman <shadman.wasif@gmail.com>
2024-03-12 14:42:35 +0600

    Customize the serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only= True)
+    edit_url = serializers.SerializerMethodField(read_only= True)
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
+            'email',
             'pk',
             'title',
             'content',
@@ -14,6 +21,27 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+        
+    def create(self, validated_data):
+        # return Product.objects.create(**validated_data)
+        # email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        # print(email)
+        # print(obj)
+        return obj
+    
+    def update(self, instance, validated_data):
+        email = validated_data.pop('emaill')
+        return super().update(instance, validated_data)
+        
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        
+        print(obj)
+        
+        return reverse("product-edit", kwargs={'pk': obj.pk}, request=request)
         
     def get_my_discount(self, obj):
         # obj.user -> I can get user.username
