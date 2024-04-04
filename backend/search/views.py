ff79cc456f9fc599a8ef7725b3a8db5a4f75b707
Kingsman <shadman.wasif@gmail.com>
2024-04-04 16:57:51 +0600

    Algolia Client Building

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.response import Response
+
+from . import client
+class SearchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query)
+        return Response(results)
 
 
-class SearchListView(generics.ListAPIView):
+class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
