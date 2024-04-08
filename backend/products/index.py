825b52e9ea2e66099661e539b89d2a8cc4b4f38b
Kingsman <shadman.wasif@gmail.com>
2024-04-08 14:04:45 +0600

    Agolia Search Client for Django

--- backend/products/index.py
+++ backend/products/index.py
@@ -5,8 +5,12 @@ from .models import Product
 
 @register(Product)
 class ProductIndex(AlgoliaIndex):
-    should_index = 'is_public'
+    # should_index = 'is_public'
     fields = ['title', 'content', 'price', 'user', 'public']
+    settings = {
+        'searchableAttributes': ['title', 'content'],
+        'attributesForFaceting': ['user', 'public']
+    }
     tags = 'get_tags_list'
 
     
