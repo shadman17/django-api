911b2120bcb8568e38345fd9646de140abdc6aaf
Kingsman <shadman.wasif@gmail.com>
2024-04-17 15:57:21 +0600

    Creation of a JS client

---
+++ js_client/client.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById("login-form")
+const baseEndpoint = "http://localhost:8000/api"
+
+
+if (loginForm){
+    loginForm.addEventListener('submit', handleLogin)
+
+}
+
+function handleLogin(e){
+    // console.log(e)
+    e.preventDefault()
+    const loginEndpoint = `${baseEndpoint}/token/`
+    let loginFormData = new FormData(loginForm)
+    let loginObjectData = Object.fromEntries(loginFormData)
+    let bodyStr = JSON.stringify(loginObjectData)
+    const options = {
+        method: "POST",
+        headers: {
+            "ContentType": "application/json"
+        },
+        body: bodyStr
+
+    }
+    fetch(loginEndpoint, options)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
